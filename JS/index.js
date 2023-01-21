8de06481ed03bde78a8a60e09fd13d5d54bc8579
Uriel Ortiz <urielortizlerma@gmail.com>
2023-01-20 19:32:08 -0600

    Mostrar Peliculas Estrellas

--- JS/index.js
+++ JS/index.js
@@ -5,13 +5,23 @@ fetch(`${urlApi}movie/now_playing?api_key=e0608bd99c0ab55b22dd875841bed505&langu
     let tarjetas = '';
     const {results} = finalResp; 
     console.log(results[0]);
-    results.forEach(({title, release_date, overview, poster_path }) => {
+    results.forEach(({title, release_date, overview, poster_path,vote_average }) => {
+      let clasificacion = '';
+      for(let i = 1; i<=Math.round(vote_average); i++){
+        clasificacion+=` <strong><i class="bi bi-star-fill amarillo"></i></strong>`;
+      }
+      let estrellasVacias= 9 - Math.round(vote_average);
+      let clasificacionVacio = '';
+      for(let i = 0; i<=estrellasVacias; i++){
+        clasificacionVacio+=` <strong><i class="bi bi-star"></i></strong>`;
+      }
         tarjetas+=  `<div class="col">
         <div class="card h-100">
           <img height="500" src="https://image.tmdb.org/t/p/w220_and_h330_face/${poster_path}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${title}</h5>
             <p>Fecha de salida: <strong>${fechaFormatoMX(release_date)}</strong></p>
+            <p>Valoración: ${clasificacion} ${clasificacionVacio}</p>
           </div>
         </div>
       </div>`;
@@ -20,4 +30,6 @@ fetch(`${urlApi}movie/now_playing?api_key=e0608bd99c0ab55b22dd875841bed505&langu
     document.getElementById("tarjetasPeliculas").innerHTML = tarjetas
 })
 
- 
+
+  
+
